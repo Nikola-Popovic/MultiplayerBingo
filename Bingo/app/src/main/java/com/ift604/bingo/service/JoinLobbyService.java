91de100a55dad0ed4cebd2323219112ccde88433
Alexandre <alexandre.peloquin.emond@usherbrooke.ca>
2020-11-22 09:41:33 -0500

    popup size, fix createLobbyReceiver

--- Bingo/app/src/main/java/com/ift604/bingo/service/JoinLobbyService.java
+++ Bingo/app/src/main/java/com/ift604/bingo/service/JoinLobbyService.java
@@ -4,6 +4,11 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
 
+import androidx.annotation.Nullable;
+
+import com.ift604.bingo.dal.Datasource;
+import com.ift604.bingo.model.Lobby;
+
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
@@ -12,14 +17,23 @@ import android.content.Context;
  * helper methods.
  */
 public class JoinLobbyService extends IntentService {
-
+    public static final String JOIN_LOBBY_ACTION = "JOIN_LOBBY_ACTION";
+    public static final String JOIN_LOBBY_EXTRA = "JOIN_LOBBY_EXTRA";
+    Datasource bingoRepository;
     public JoinLobbyService() {
         super("JoinLobbyService");
+        bingoRepository = new Datasource();
     }
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
 
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+        Lobby lobby = bingoRepository.createLobby(0);
+        Intent i = new Intent();
+        i.setAction(JOIN_LOBBY_ACTION);
+        i.putExtra(JOIN_LOBBY_EXTRA, lobby);
+        sendBroadcast(i);
     }
 
 }
