213684c925e82e4b452f2f58af1ecaed3cb8c114
Nikola Popovic <popovic.k.nikola@gmail.com>
2020-11-22 22:57:51 -0500

    Room Finder Design Update

--- Bingo/app/src/main/java/com/ift604/bingo/LobbyItemAdapter.java
+++ Bingo/app/src/main/java/com/ift604/bingo/LobbyItemAdapter.java
@@ -1,12 +1,15 @@
 package com.ift604.bingo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ift604.bingo.model.Lobby;
@@ -21,11 +24,14 @@ public class LobbyItemAdapter extends RecyclerView.Adapter<LobbyItemAdapter.View
         this.lobbies = matches;
     }
 
+    private Context context;
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_lobby_item, parent, false);
+        context = parent.getContext();
         return new ViewHolder(view);
     }
 
@@ -33,6 +39,7 @@ public class LobbyItemAdapter extends RecyclerView.Adapter<LobbyItemAdapter.View
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final Lobby item = lobbies.get(position);
         populateData(holder, item);
+        setBackGround(holder, position);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +47,16 @@ public class LobbyItemAdapter extends RecyclerView.Adapter<LobbyItemAdapter.View
                 //TODO INFLATE WITH WAIT LOBBY FRAGMENT
             }
         });
+    }
+
+    private void setBackGround(@NonNull ViewHolder holder, int position) {
+        if(position % 4 == 0 ){
+            holder.layout.setBackground(ContextCompat.getDrawable(context, R.drawable.white_btn));
+        } else if (position % 4 == 1 || position % 4 == 3) {
+            holder.layout.setBackground(ContextCompat.getDrawable(context, R.drawable.btn_peach));
+        } else {
+            holder.layout.setBackground(ContextCompat.getDrawable(context, R.drawable.btn_orangy));
+        }
     }
 
     private void populateData(@NonNull ViewHolder holder, Lobby item) {
@@ -55,12 +72,13 @@ public class LobbyItemAdapter extends RecyclerView.Adapter<LobbyItemAdapter.View
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView lobbyName;
         TextView playerAmount;
-
+        LinearLayout layout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             lobbyName = itemView.findViewById(R.id.lobby_item_name);
             playerAmount = itemView.findViewById(R.id.lobby_item_player_amount);
+            layout = itemView.findViewById(R.id.lobby_item_layout);
         }
     }
 
