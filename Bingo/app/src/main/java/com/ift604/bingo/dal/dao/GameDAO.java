4c72f2c576604b8597db677e287e99fc5999af90
Alexandre <alexandre.peloquin.emond@usherbrooke.ca>
2020-11-22 21:07:27 -0500

    Idem

--- Bingo/app/src/main/java/com/ift604/bingo/dal/dao/GameDAO.java
+++ Bingo/app/src/main/java/com/ift604/bingo/dal/dao/GameDAO.java
@@ -1,9 +1,14 @@
-package com.ift604.bingo.dal.datamapper;
+package com.ift604.bingo.dal.dao;
 
-import com.ift604.bingo.dal.BingoServerDatasource;
+import com.ift604.bingo.DummyCard;
+import com.ift604.bingo.model.Card;
 
 public class GameDAO extends GenericDataHandler {
-    public GameDAO(BingoServerDatasource bingoServerDatasource) {
-        super(bingoServerDatasource);
+    public GameDAO() {
+        super();
+    }
+
+    public Card getPlayerCard() {
+        return DummyCard.generateDummyCard();
     }
 }
--- Bingo/app/src/main/java/com/ift604/bingo/dal/dao/LobbyDAO.java
+++ Bingo/app/src/main/java/com/ift604/bingo/dal/dao/LobbyDAO.java
@@ -1,9 +1,52 @@
-package com.ift604.bingo.dal.datamapper;
+package com.ift604.bingo.dal.dao;
 
-import com.ift604.bingo.dal.BingoServerDatasource;
+import com.ift604.bingo.DummyCard;
+import com.ift604.bingo.model.Lobby;
+import com.ift604.bingo.model.Participant;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LobbyDAO extends GenericDataHandler {
-    public LobbyDAO(BingoServerDatasource bingoServerDatasource) {
-        super(bingoServerDatasource);
+    LobbyDatamapper lobbyMapper;
+    private final String lobbyPath = "/lobby";
+    private final String userPath = lobbyPath + "/joueur";
+    public LobbyDAO() {
+        super();
+        lobbyMapper = new LobbyDatamapper();
+    }
+
+    public ArrayList<Lobby> getLobbiesNearMe() {
+        return DummyCard.dummyLobby();
+    }
+
+    public ArrayList<Participant> getLobbyParticipants(int lobbyId) {
+        return (ArrayList<Participant>) DummyCard.dummyLobby().get(0).getParticipants();
+    }
+
+    public Lobby createLobby(int hostId, String name) throws IOException, JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("hostId", hostId);
+      //  jsonObject.put("name", name);
+        String jsonLobby = postDataToUrl(lobbyPath, jsonObject);
+        return lobbyMapper.buildLobby(jsonLobby);
+    }
+
+    public void addPersonToLobby(int lobbyId, int userId) {
+        //TODO DO PUT
+    }
+
+    public Participant createUser() {
+        String url = userPath;
+        try {
+            String jsonReturnValue = postDataToUrl(url, new JSONObject());
+            return lobbyMapper.mapUserToJson(jsonReturnValue);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
