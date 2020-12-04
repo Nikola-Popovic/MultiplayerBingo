59752b437eb9acac66a8747c6443ffc156c3fe59
Nikola Popovic <popovic.k.nikola@gmail.com>
2020-12-04 16:46:22 -0500

    Link start game button with backend

--- frontend/Bingo/app/src/main/java/com/ift604/bingo/dal/dao/GameDAO.java
+++ frontend/Bingo/app/src/main/java/com/ift604/bingo/dal/dao/GameDAO.java
@@ -1,10 +1,20 @@
 package com.ift604.bingo.dal.dao;
 
+import android.util.Log;
+import android.widget.Toast;
+
+import com.androidnetworking.common.ANResponse;
 import com.ift604.bingo.fel.DummyCard;
 import com.ift604.bingo.model.Card;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class GameDAO extends GenericDataHandler {
     GameDatamapper gameDatamapper;
+    private final String lobbyPath = "/lobby";
     private final String gamePath = "/game";
     private final String userPath = gamePath + "/joueur";
     public GameDAO() {
@@ -16,13 +26,21 @@ public class GameDAO extends GenericDataHandler {
     }
 
     public void startGame(int lobbyId, int hostId) {
-//        String url = lobbyPath;
-//        try {
-//            String result = postDataToUrl(url,);
-//            return gameDatamapper.buildGame(result);
-//        } catch (IOException e) {
-//            return new ArrayList<>();
-//        }
+        try {
+            String url = String.format("%s/%s/start", lobbyPath, String.valueOf(lobbyId));
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("hostId", hostId);
+            ANResponse response = postDataToUrl(url, jsonObject);
+
+            if(response.isSuccess()) {
+                // Great !
+                String message = String.format("Game %s started.", lobbyId);
+                Log.i("START_GAME_SUCCESS", message);
+                // What to do
+            } else {
+                Log.e("START_GAME_FAILED", response.getError().getMessage(), response.getError().getCause());
+            }
+       } catch (JSONException j) {}
     }
 
     public String getPreviousNumber() {
