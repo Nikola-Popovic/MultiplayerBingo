30bf7f0125a8a7dcea889142f1c042e07df015c0
Francis Hamel <francishamel96@gmail.com>
2020-12-05 19:03:24 -0500

    Implement more messages

--- backend/src/messaging.ts
+++ backend/src/messaging.ts
@@ -1,18 +1,44 @@
 import firebase from './firebase-config';
 import Carte from './models/carte';
+import Joueur from './models/joueur';
 
 export function sendCardToTokens(tokens : string[], lobbyId : number) {
   tokens.forEach(token => {
     firebase.messaging().sendToDevice(token, {
-      data: {
-        "type": "carte",
-        "payload": JSON.stringify(new Carte(lobbyId).toJSON())
-      }
+      data: formatMessage("carte", JSON.stringify(new Carte(lobbyId).toJSON()))
     });
   });
 }
 
-export function subscribeTokensToLobbyTopic(tokens : string[], lobbyId : number) {
-  const topic = `lobby${lobbyId}Topic`;
-  firebase.messaging().subscribeToTopic(tokens, topic);
+export function subscribeTokenToLobbyTopic(token : string, lobbyId : number) {
+  firebase.messaging().subscribeToTopic(token, getLobbyTopic(lobbyId));
+}
+
+export function sendAddedPlayerMessageToLobby(joueur : Joueur, lobbyId : number) {
+  firebase.messaging().sendToTopic(getLobbyTopic(lobbyId), {
+    data: formatMessage("addedPlayer", JSON.stringify(joueur.toJSON()))
+  });
+}
+
+export function sendRemovedPlayerMessageToLobby(joueur : Joueur, lobbyId : number) {
+  firebase.messaging().sendToTopic(getLobbyTopic(lobbyId), {
+    data: formatMessage("removedPlayer", JSON.stringify(joueur.toJSON()))
+  });
+}
+
+export function sendNextNumberToLobby(nextNumber : number, lobbyId : number) {
+  firebase.messaging().sendToTopic(getLobbyTopic(lobbyId), {
+    data: formatMessage("nextNumber", nextNumber.toString())
+  })
+}
+
+function getLobbyTopic(lobbyId : number) {
+  return `lobby${lobbyId}Topic`;
+}
+
+function formatMessage(type : string, payload : string) : { "type" : string, "payload" : string} {
+  return {
+    "type": type,
+    "payload": payload
+  }
 }
